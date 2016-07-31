fbb0195edd1a082de9a4a613b322f4f8dd995ab8
Bertram Felgenhauer <int-e@gmx.de>
2016-07-31 12:08:52 +0200

    polish; add A111042

--- a059756.cc
+++ a059756.cc
@@ -1,12 +1,29 @@
-//  g++ -O2 -Wall q.cc -lminisat
+// Compute Erdős-Woods numbers (cf. https://oeis.org/A059756)
+//
+// n is an EW number if the primes up to n can be partitioned into two
+// sets A and B such that for all m in [1,n-1], either m is divisible
+// by a prime in A or (n-m) is divisible by a prime in B. This is easily
+// encoded as a SAT problem using two variables per prime.
+//
+// Author: Bertram Felgenhauer <int-e@gmx.de>
+//
+// 2016-07-30/31: initial version
+//
+// to compile (needs MiniSat): g++ -O2 -Wall q.cc -lminisat
 
-#include <minisat/simp/SimpSolver.h>
+// Note: It would be interesting to also cover https://oeis.org/A059757.
+// That would require enumerating all solutions and doing some slightly
+// tedious CRT computation.
+
 #include <cstdlib>
 #include <iostream>
+#include <minisat/simp/SimpSolver.h>
 
 using namespace Minisat;
 
-int p[1<<18];
+// compute prime numbers (naive)
+
+int p[1<<18]; // good up to 3681131
 int l;
 
 static void init_primes(int n)
@@ -28,24 +45,29 @@ static void init_primes(int n)
     }
 }
 
+// check whether n is an EW number
 static bool solve(int n)
 {
     SimpSolver s;
     Var v[2*l];
     for (int i = 0; i < l && p[i] < n; i++) {
+        // allocate two variables per prime
         v[2*i] = s.newVar();
         v[2*i+1] = s.newVar();
+        // assert that at most one of them is set
         if (!s.addClause(~mkLit(v[2*i]), ~mkLit(v[2*i+1])))
             return false;
     }
+    // prepare clauses for each m by enumerating multiples (like sieving)
     vec<Lit> c[n];
-    for (int j = 0; j < l; j++) {
+    for (int j = 0; j < l && p[j] < n; j++) {
         int q = p[j];
         for (int i = q; i < n; i += q)
             c[i].push(mkLit(v[2*j]));
         for (int i = n-q; i > 0; i -= q)
             c[i].push(mkLit(v[2*j+1]));
     }
+    // assert clauses and solve
     for (int i = 1; i < n; i++) {
         if (!s.addClause_(c[i]))
             return false;
